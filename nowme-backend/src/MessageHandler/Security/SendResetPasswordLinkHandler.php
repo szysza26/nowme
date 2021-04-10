d5ef814f8dbde8a45410368220de19cb6df7ab9c
yell0w <kontakt@patryksieradzki.pl>
2021-04-10 12:51:50 +0200

    feature(security) add reset password token generator

--- nowme-backend/src/MessageHandler/Security/SendResetPasswordLinkHandler.php
+++ nowme-backend/src/MessageHandler/Security/SendResetPasswordLinkHandler.php
@@ -5,10 +5,23 @@ declare(strict_types=1);
 namespace NowMe\MessageHandler\Security;
 
 use NowMe\Message\Security\SendResetPasswordLink;
+use NowMe\Service\Security\ResetPasswordTokenGenerator;
+use Symfony\Component\Mailer\MailerInterface;
 
 final class SendResetPasswordLinkHandler
 {
+    public function __construct(private MailerInterface $mailer, private ResetPasswordTokenGenerator $passwordTokenGenerator, private $users)
+    {
+        
+    }
     public function __invoke(SendResetPasswordLink $message)
     {
+        if (!$this->users->emailExist($message->email())) {
+            return;
+        }
+
+        $token = $this->passwordTokenGenerator->generate();
+        $this->users->getByEmail($message->email())->setResetPasswordToken($token);
+        $this->mailer->send();
     }
 }
