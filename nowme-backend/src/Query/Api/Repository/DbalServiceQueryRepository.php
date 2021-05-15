ca91aaf8cefb2f5e7823edecd8e88bcb7249f2c8
yell0w <kontakt@patryksieradzki.pl>
2021-05-15 09:36:56 +0200

    Add search details

--- nowme-backend/src/Query/Api/Repository/DbalServiceQueryRepository.php
+++ nowme-backend/src/Query/Api/Repository/DbalServiceQueryRepository.php
@@ -63,4 +63,57 @@ final class DbalServiceQueryRepository implements ServiceQueryRepository
 
         return $result;
     }
+
+    public function details(array $data): array
+    {
+        $qb = $this->connection->createQueryBuilder();
+
+        $qb->select(
+            [
+                'a.date',
+                'a.hour_from',
+                'a.hour_to',
+            ]
+        )
+            ->from('availability', 'a')
+            ->leftJoin('a', 'user', 'u', 'a.specjalist_id = u.id')
+            ->leftJoin('u', 'user_office', 'uf', 'u.id = uf.user_id')
+            ->leftJoin('uf', 'office', 'o', 'uf.office_id = o.id')
+            ->leftJoin('u', 'service', 's', 'a.specjalist_id = s.specialist_id')
+            ->innerJoin('s', 'service_dictionary', 'sd', 's.name_id = sd.id');
+
+        if ($data['specialist']) {
+            $qb->andWhere($qb->expr()->eq('u.id', ':specialist'))
+                ->setParameter('specialist', $data['specialist']);
+        }
+
+        if ($data['service']) {
+            $qb->andWhere($qb->expr()->eq('sd.id', ':service'))
+                ->setParameter('service', $data['service']);
+        }
+
+        if ($data['office']) {
+            $qb->andWhere($qb->expr()->eq('o.id', ':office'))
+                ->setParameter('office', $data['office']);
+        }
+
+        if ($data['date_from']) {
+            $qb->andWhere($qb->expr()->gte('a.date', ':date_from'))
+                ->setParameter('date_from', $data['date_from']);
+        }
+
+        if ($data['date_to']) {
+            $qb->andWhere($qb->expr()->lte('a.date', ':date_to'))
+                ->setParameter('date_to', $data['date_to']);
+        }
+
+        $qb->groupBy('u.id', 'o.id', 's.id', 'a.id');
+
+        dump($qb->getSQL());
+
+        $result = $qb->execute()
+            ->fetchAllAssociative();
+
+        return $result;
+    }
 }
