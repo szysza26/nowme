22c599951a78872732b12a3541bc9ae12e080f3f
michalobielak <om45855@zut.edu.pl>
2021-05-16 13:46:14 +0200

    payment

---
+++ nowme-backend/src/Controller/Api/PaymentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NowMe\Controller\Api;
+
+use http\Env\Request;
+use NowMe\Repository\PaymentRepository;
+use NowMe\Service\PayU;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaymentController extends AbstractApiController
+{
+    private PaymentRepository $paymentRepository;
+
+    public function __construct(PaymentRepository $paymentRepository)
+    {
+        $this->paymentRepository = $paymentRepository;
+    }
+
+    #[Route('/payment', name: 'payment', methods: ['GET'])]
+    public function index(Request $request): Response
+    {
+        $payments = $this->paymentRepository->getAllAdd();
+
+        $payU = new PayU();
+        foreach ($payments as $payment) {
+            $payU->notify($payment);
+            $this->paymentRepository->save($payment);
+        }
+        return $this->json(['status' => 'OK']);
+    }
+}
